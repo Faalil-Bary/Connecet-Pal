b335f9bfa1879c0cec7ed0a238a4150c37414eec
faalil-bary <muhammadhfaalil@gmail.com>
2023-06-15 09:49:43 +0530

    Navbar , Sidebars implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -8,9 +9,60 @@ import {
 
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import Navbar from "./components/navBar/navBar";
+import LeftBar from "./components/leftBar/leftBar";
+import RightBar from "./components/rightBar/rightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import "./styles.scss";
 
 function App() {
+
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div className="theme-light">
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
     {
       path: "/login",
       element: <Login />,
